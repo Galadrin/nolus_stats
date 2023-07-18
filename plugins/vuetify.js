9799cb2242316d7d4ddff4eab9bb518e717f6623
David Pierret <dapie@cros-nest.com>
2023-07-19 01:28:24 +0200

    improve loan explorer

--- plugins/vuetify.js
+++ plugins/vuetify.js
@@ -2,13 +2,16 @@
 import '@mdi/font/css/materialdesignicons.css'
 import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
+
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 
 export default defineNuxtPlugin(nuxtApp => {
   const vuetify = createVuetify({
     ssr: true,
-    components,
+    components: {
+      ...components,
+    },
     directives,
     customVariables: ['~/assets/variables.scss'],
     theme: {
